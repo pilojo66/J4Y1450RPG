9391c8de3c3db1a85f43267478ef441e0f7d5139
J4Y1450 <J4Y1450@hotmail.com>
2015-07-16 16:16:19 -0400

    started header for mage

---
+++ include/Player/Mage.h
@@ -0,0 +1,24 @@
+#ifndef MAGE_H
+#define MAGE_H
+#include "Player.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+class Mage: public Player{
+	
+public:
+	Mage();
+
+	void TakeDamage(int);
+	void TakeBurn(int);
+	void TakeStun(int);
+	bool UseItem(void);
+
+	//ABILITIES
+	int Attack(void);
+	int Fireball(void);
+	int IceShield(void);
+	int Conversion(void);
+};
+#endif
